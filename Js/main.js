fa4d628d1a06b85fe504f8aea775341ca16fda2a
Marcelo Soares <marcelohsoares142@gmail.com>
2024-06-20 18:27:11 -0300

    ajustes finais

---
+++ Js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 0000-0000');
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            txtarea: {
+                required: true
+            }
+        }
+    })
+})
